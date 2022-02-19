0c54dfddda77538df464bd2eaf673e458bf563ad
Ben Mueller <benjm2002@gmail.com>
2022-02-19 12:36:33 -0600

    can now create listings that will be saved to database

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    startingBid = models.FloatField()
+    category = models.CharField(max_length=100)
+    image = models.URLField()
--- auctions/views.py
+++ auctions/views.py
@@ -3,8 +3,18 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django import forms
+
+from . import models
 
 from .models import User
+
+class newListingForm(forms.Form):
+    title = forms.CharField(label="Listing title")
+    description = forms.CharField(label="Listing description", widget=forms.Textarea(attrs={"rows":5, "cols":60}))
+    startingBid = forms.FloatField(label="Starting Bid", min_value = 0.01)
+    category = forms.CharField(label="Category", required=False)   # could change this to a choice field if we want
+    image = forms.URLField(label="Image URL", required=False)
 
 
 def index(request):
@@ -27,7 +37,7 @@ def login_view(request):
             return render(request, "auctions/login.html", {
                 "message": "Invalid username and/or password."
             })
-    else:
+    else:  # if request.method == "GET"
         return render(request, "auctions/login.html")
 
 
@@ -61,3 +71,23 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
+
+def createListing(request):
+    if request.method == "POST":
+        # need to make a copy of request.POST without the csrfmiddlewaretoken entry (can't just delete bc request.POST is immutable)
+        data = {}
+        for k in request.POST.keys():
+            if k != "csrfmiddlewaretoken":
+                data[k] = request.POST[k]
+        listing = models.Listing(**data)  # can you pass a dictionary in? or do I have to pass in each field individually? YES! If we use **
+        listing.save()
+        return(HttpResponseRedirect(reverse("index")))
+
+    else:
+        form = newListingForm()
+        context = {"form": form}
+        return render(request, "auctions/newListing.html", context)
+
+
